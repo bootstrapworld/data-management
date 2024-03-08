295212d46be581e2345514f0643dce4631b8940a
er1c456 <eric@bootstrapworld.org>
2024-03-08 09:27:51 -0700

    Moved user

--- appsscript/create_tables.ts
+++ appsscript/create_tables.ts
@@ -13,22 +13,6 @@ const tables = [
 ];
 console.log(tables.length);
 
-const user = [
-    "id",                  //SERIAL PRIMARY KEY, --TEXT
-    "name_first",          //TEX -- the legal first name, i.e. would appear on DL
-    "name_last",           //TEXT
-    "name_first_alt",      //TEXT, -- usually shortened nick name, e.g. David -> Dave
-    "name_last_alt",       //TEXT, -- usually for married name change
-    "email_personal",      //TEXT, -- email that persists after job change
-    "email_professional",  //TEXT, -- email that changes with job change
-    "home_location",       //INTEGER REFERENCES "location",
-    "home_phone",          //varchar(15),
-    "cell_phone",          //varchar(15)
-    "work_phone",          //varchar(15)
-    "user_gender",         //     GENDER
-    "user_race",            //     RACE
-    "user_employer"        //TEXT
-];
 
 // consumes an array of table names, creates a new sheet if the given sheet name does not exist 
 const createSheets = function(t) {
@@ -52,8 +36,6 @@ const dropSheets = function() {
     var sheets = ss.getSheets();
     sheets.forEach((v) => ss.deleteSheet(v));
 }
-
-createSheets(tables);
 
 class Employee {
     empCode: number;
