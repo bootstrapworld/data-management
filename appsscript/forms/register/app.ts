b0ddb6b951187dce5e26020cef78bcb3524312b2
er1c456 <eric@bootstrapworld.org>
2024-03-14 20:06:56 -0600

    attempt at clearForm()

--- appsscript/forms/register/app.ts
+++ appsscript/forms/register/app.ts
@@ -25,24 +25,26 @@ function getForm() {
         console.log('Failed with error: %s', err.message);
     }
 }
-
-function getDestination() {
+// -> Spreadsheet 
+function setDestination() {
     try {
         // Check if the property is not null
         const scriptProperties = PropertiesService.getScriptProperties();
-        const id = scriptProperties.getProperty('DEST_ID');
-        if (id == undefined) {
+        const formId = scriptProperties.getProperty('FORM_ID');
+        const destId = scriptProperties.getProperty('DEST_ID');
+        const form = FormApp.openById(formId);
+        if (destId == undefined) {
             const dest = SpreadsheetApp.create("db");
             const id = dest.getId();
             console.log('New destination created with id: %s', id);
             scriptProperties.setProperty('DEST_ID', id);
             console.log('New destination id property added: %s', id);
-            const name = dest.getName();
-            console.log('Title: %s', name);
             return dest;
         } else {
             const dest = SpreadsheetApp.openById(id);
             console.log('Destination id: %s', dest.getId());
+            form.setDestination(FormApp.DestinationType.SPREADSHEET, destId);
+            console.log('Destination added to form');
             return dest;
         }
     } catch (err) {
@@ -64,6 +66,12 @@ function setTitle() {
     return title;
 }
 
+function setupForm() {
+    var form = getForm();
+    form.setDescription('Registering as a user for Bootstrap. As a Bootstrap user you have access to protected curriculum materials, join our mailing list, and can enroll in events')
+        .setConfirmationMessage('Thank you for registering with Bootstrap. You will get a confirmation message in the next 24 hours after your registration has been confirmed.')
+}
+
 /**
 for container bound script
 function getActiveForm() {
@@ -78,9 +86,52 @@ function logFormInfo() {
     var title = form.getTitle();
     console.log(`Form Id: ${id}` + "\n" + `Title: ${title}`);
 }
+// clear form
+function clear() {
+    try {
+        const form = getForm();
+        const items = form.getItems();
+        const len = items.length;
+        console.log('Number of items: %s', len);
+        form.deleteItem(items[0]);
+        console.log('Number of items: %s', len);
+
+    } catch (err) {
+        console.log('Failed with error: %s', err.message);
+    }
+}
 
 // Form Questions
+// First time enrolling?
+function buildForm() {
+    const form = getForm();
+    (function() {
+        const item = form.addMultipleChoiceItem();
+        item.setTitle("First time registering?")
+            .setHelpText("Is this your first time registering with Bootstrap?")
+            .setChoices([
+                item.createChoice("Yes", whyRegistering),
+                item.createChoice("No", FormApp.PageNavigationType.SUBMIT)]);
+    })();
+    const FirstTimeRegistering = form.addPageBreakItem().setTitle('');
+    (function() {
+        form.d
+    })
+    const whyRegistering = form.addPageBreakItem().setTitle('Why are you registering with Bootstrap?');
+    (function() {
+        const item = form.addMultipleChoiceItem();
+        item.setTitle("First time registering?")
+            .setHelpText("Is this your first time registering with Bootstrap?")
+            .setChoices([
+                item.createChoice("Yes", identity),
+                item.createChoice("No", FormApp.PageNavigationType.SUBMIT)]);
+    })();
 
+    const identity = form.addPageBreakItem().setTitle('Identity');
+    const contact = form.addPageBreakItem().setTitle('Contact Information');
+
+
+}
 
 // Data Piping
 
